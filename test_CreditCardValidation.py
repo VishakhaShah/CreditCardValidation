592f3d58b868d28199f6b17b6e56abd4b54518cd
vishakha <vishakha.0503@gmail.com>
2020-01-24 16:16:13 +0530

    Added test case

--- test_CreditCardValidation.py
+++ test_CreditCardValidation.py
@@ -1,3 +1,12 @@
-import CreditCardValidation
-def test_Validations():
+execfile("/Users/shahvishakharadhamohan/Documents/CreditCardvalidation.py")
+def test_card_Validations():
     assert validate(get_cc_number()) == True
+
+def test_cardExpiry():
+    assert validate_date() == True
+
+def test_card_cvv():
+    assert validate_cvv() == True
+
+def card_name():
+    assert validate_name() == True
